e6f1799b906dd5bb3d5eb8a4e77bd05f10863975
williammlekush <56037586+williammlekush@users.noreply.github.com>
2022-04-12 13:40:09 +0400

    chore: copy previous code

--- 03_assignment1/src/ofApp.h
+++ 03_assignment1/src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxGui.h"
 
 class ofApp : public ofBaseApp{
 
@@ -11,7 +12,7 @@ class ofApp : public ofBaseApp{
 
 		void keyPressed(int key);
 		void keyReleased(int key);
-		void mouseMoved(int x, int y );
+		void mouseMoved(int x, int y);
 		void mouseDragged(int x, int y, int button);
 		void mousePressed(int x, int y, int button);
 		void mouseReleased(int x, int y, int button);
@@ -20,5 +21,29 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+
+		// line moving
+		ofPolyline activeLine;
+		// bin for static lines, enables screen wrapping
+		vector<ofPolyline> lines;
+
+		// position vector
+		ofVec2f pos;
+
+		// speed of movement
+		int speed;
+
+		// position change variables
+		int xChange;
+		int yChange;
+
+		// gui variables
+		ofxPanel gui;
+		ofxFloatSlider thickSlider;
+		ofxLabel rotateLabel;
+		ofxColorSlider colorSlider;
+
+		// screenshot variables
+		ofImage screenshot;
 		
 };
